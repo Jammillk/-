a2f74d4a9c1147f81ee67f9e0b14cd2ef94ea50b
Jammillk <im@tanjiaming99.com>
2021-06-06 22:56:54 +0800

    使用Promise封装request请求，以免日后代码的烦杂

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,6 @@
-//Page Object
+// 引入用来发送请求的方法，要把路径补全！
+import { request } from "../../request/index.js";
+
 Page({
   data: {
     // 轮播图数组
@@ -7,13 +9,21 @@ Page({
   // 页面开始加载的时候就会触发的事件
   onLoad: function(options){
     // 1. 发送异步请求获取轮播图数据
-    wx.request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
-      success: (result)=>{
+    // 优先的手段可以通过es6的promise来解决
+    // wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    //   success: (result)=>{
+    //     this.setData({
+    //       swiperList: result.data.message
+    //     })
+    //   }
+    // });
+    // 开启ES6转ES5才成功，看来这些还是有、、问题的
+    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"})
+    .then(result=>{
         this.setData({
           swiperList: result.data.message
         })
-      }
-    });
+    })
   }
 });
