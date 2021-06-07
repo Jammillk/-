624c97ed4fbf213391d56d31bf9f93dd364356e0
Jammillk <im@tanjiaming99.com>
2021-06-07 14:24:54 +0800

    商品列表：自定义组件tab，并实现了切换功能

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,17 +1,43 @@
 // pages/goods_list/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    tabs: [{
+      id: 0,
+      value: "综合",
+      isActive: true
 
+    }, {
+      id: 1,
+      value: "销量",
+      isActive: false
+
+    }, {
+      id: 2,
+      value: "价格",
+      isActive: false
+
+    }, ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options);
-  } 
+  },
+  // 标题点击事件
+  handleTabsItemChange(e) {
+    // 获取被点击的标题索引
+    const {
+      index
+    } = e.detail;
+    // 修改源数组
+    let {
+      tabs
+    } = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+    // 赋值到data中
+    this.setData({
+      tabs
+    })
+  }
 })
