eb314b9d385bbed3fa23f5a5a10d3509c32a9752
Jammillk <im@tanjiaming99.com>
2021-06-07 10:35:17 +0800

    商品分类：从接口中获得商品数据

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,20 +1,43 @@
 // pages/category/index.js
+import {
+  request
+} from "../../request/index.js";
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 左侧菜单数据
+    leftMenuList: [],
+    // 右侧商品数据
+    rightContent: []
   },
+  // 接口的返回数据
+  Cates: [],
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCates();
   },
-  getCates(){
-    
+  getCates() {
+    request({
+        url: "https://api-hmugo-web.itheima.net/api/public/v1/categories"
+      })
+      .then(res => {
+        // console.log(res)
+        this.Cates = res.data.message
+        // 构造左侧大菜单数据
+        let leftMenuList = this.Cates.map(v=>v.cat_name)
+        // 构造右侧商品数据
+        let rightContent = this.Cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+      })
   }
 })
