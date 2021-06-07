56e61257ec1530f4fd918842b769ea791c91b63b
Jammillk <im@tanjiaming99.com>
2021-06-07 18:40:57 +0800

    商品详情：优化数据体积，只取需要的；前端临时修改，让webp格式转成jpg

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -11,21 +11,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-    goodsObj:{}
+    goodsObj: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const {goods_id} = options
+    const {
+      goods_id
+    } = options
     this.getGoodsDetail(goods_id)
-  }, 
+  },
   // 获取商品的详情数据
-  async getGoodsDetail(goods_id){
-    const goodsObj = await request({url:"/goods/detail",data:{goods_id}})
+  async getGoodsDetail(goods_id) {
+    const goodsObj = await request({
+      url: "/goods/detail",
+      data: {
+        goods_id
+      }
+    })
     this.setData({
-      goodsObj
+      goodsObj: {
+        goods_name: goodsObj.goods_name,
+        goods_price: goodsObj.goods_price,
+        pics: goodsObj.pics,
+        // iphone部分手机不识别webp图片格式，
+        // 前端可以临时自己改，要确保后台存在别的格式的图片
+        goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g,'.jpg'),
+      }
     })
   }
 })
